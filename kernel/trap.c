cdb21acce97f09ac45269d7b975163ebc04282c8
gengmeng <gengmeng2019@163.com>
2022-04-01 22:03:59 +0800

    alarm test0

--- kernel/trap.c
+++ kernel/trap.c
@@ -77,8 +77,20 @@ usertrap(void)
     exit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2)
+  if(which_dev == 2){
+    if(p->ticks==p->interval){
+      p->ticks=0;
+      p->trapframe->epc=p->handler;
+
+    }else{
+      p->ticks++;
+    }
     yield();
+
+
+  }
+
+    
 
   usertrapret();
 }
