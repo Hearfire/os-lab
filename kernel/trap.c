fb2ca22c48173b7d28747a5f872b3773a1593888
gengmeng <gengmeng2019@163.com>
2022-04-11 00:13:37 +0800

    lazy

--- kernel/trap.c
+++ kernel/trap.c
@@ -66,7 +66,30 @@ usertrap(void)
 
     syscall();
   } else if((which_dev = devintr()) != 0){
-    // ok
+      //OK
+  } else if(r_scause() == 13||r_scause() == 15){    
+    uint64 addr=r_stval();
+    if(p->sz<=addr||addr<p->trapframe->sp){
+      p->killed=1;
+      exit(-1);
+    }
+
+
+    uint64 a = PGROUNDDOWN(addr);
+    //printf("lazy:pagefault:%p\n",addr);
+    char *mem = kalloc();
+
+    if(mem == 0){
+      //uvmdealloc(pagetable, a, oldsz);
+      p->killed=1;
+    }else{
+      memset(mem, 0, PGSIZE);
+      if(mappages(p->pagetable, a, PGSIZE, (uint64)mem, PTE_W|PTE_R|PTE_U) != 0){
+        kfree(mem);
+        //uvmdealloc(p->pagetable, a, oldsz);
+        p->killed=1;
+      }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
